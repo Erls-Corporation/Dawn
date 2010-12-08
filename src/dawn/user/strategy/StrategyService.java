9a74813190bcc3ac4fdd53c026badcf0a3c9a741
Mike Liu <cheeze477@gmail.com>
2010-12-07 18:23:03 -0600

    removed ant; eclipse ftw

--- src/dawn/user/strategy/StrategyService.java
+++ src/dawn/user/strategy/StrategyService.java
@@ -2,26 +2,39 @@ package dawn.user.strategy;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import dawn.user.strategy.window.core.WindowCore;
 
 public class StrategyService {
+    private ExecutorService theActiveStrategiesExecutor;
     private List<Strategy> theActiveStrategies;
     
     public StrategyService() {
         theActiveStrategies = new ArrayList<Strategy>();
+        
+        this.init();
     }
     
     public void init() {
+        theActiveStrategiesExecutor = Executors.newCachedThreadPool();
+        
         WindowCore aWindowStrategy = new WindowCore();
         theActiveStrategies.add(aWindowStrategy);
     }
     
     public void start() {
-        
+        for (Strategy myStrategy : theActiveStrategies) {
+            theActiveStrategiesExecutor.execute(myStrategy);
+        }
     }
     
     public void stop() {
+        theActiveStrategiesExecutor.shutdown();
         
+        for (Strategy myStrategy : theActiveStrategies) {
+            myStrategy.stop();
+        }
     }
 }
--- src/dawn/user/strategy/window/core/WindowCore.java
+++ src/dawn/user/strategy/window/core/WindowCore.java
@@ -3,14 +3,27 @@ package dawn.user.strategy.window.core;
 import dawn.user.strategy.Strategy;
 
 public class WindowCore implements Strategy {
+    private boolean isRunning = false;
+
+    @Override
+    public void run() {
+       isRunning = true;
+       while (isRunning) {
+           step();
+           pause();
+       }
+    }
 
     @Override
     public void stop() {
         
     }
-
-    @Override
-    public void run() {
-           
+    
+    private void step() {
+        
+    }
+    
+    private void pause() {
+        
     }
 }
